11671513c8e134fd59d52f80c72a8f0b457079be
PhoB <psh0036@suwon.ac.kr>
2024-06-07 19:25:51 +0900

    [Gold IV] Title: 거짓말, Time: 220 ms, Memory: 17800 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,15 +1,17 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
 
-    public static void BFS(List<List<Integer>> parties, HashSet<Integer> truth) {
+    public static void BFS(List<List<Integer>> parties, HashSet<Integer> truth){
+
         ArrayDeque<Integer> queue = new ArrayDeque<>(truth);
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()){
             int person = queue.pop();
-            for (List<Integer> party : parties) {
+            for (List<Integer> party : parties){
                 if (party.contains(person)) {
-                    for (int p : party) {
-                        if (!truth.contains(p)) {
+                    for(int p : party){
+                        if(!truth.contains(p)){
                             queue.add(p);
                             truth.add(p);
                         }
@@ -17,45 +19,44 @@ public class Main {
                 }
             }
         }
+
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int answer = 0;
+
         HashSet<Integer> truth = new HashSet<>();
 
         String[] input = sc.nextLine().split(" ");
         int N = Integer.parseInt(input[0]);
         int M = Integer.parseInt(input[1]);
+        int answer = M;
 
-        String[] truthPerson = sc.nextLine().split(" ");
-        for (int i = 1; i < truthPerson.length; i++) {
-            truth.add(Integer.parseInt(truthPerson[i]));
+        String[] truth_person = sc.nextLine().split(" ");
+        for (int i=1; i<truth_person.length; i++){
+            truth.add(Integer.parseInt(truth_person[i]));
         }
 
+
         List<List<Integer>> parties = new ArrayList<>();
-        for (int i = 0; i < M; i++) {
+        for (int i=0; i<M; i++){
             String[] tmp = sc.nextLine().split(" ");
             List<Integer> party = new ArrayList<>();
-            for (int k = 1; k < tmp.length; k++) { // 올바른 루프 변수 사용
+            for(int k=1; k<tmp.length; k++){
                 party.add(Integer.parseInt(tmp[k]));
             }
             parties.add(party);
         }
 
-        BFS(parties, truth);
+        BFS(parties,truth);
 
-        for (List<Integer> party : parties) {
-            boolean allUnknown = true;
-            for (Integer person : party) {
-                if (truth.contains(person)) {
-                    allUnknown = false;
+        for(List<Integer> party : parties){
+            for (Integer person : party){
+                if(truth.contains(person)){
+                    answer -= 1;
                     break;
                 }
-            }
-            if (allUnknown) {
-                answer += 1;
             }
         }
 
