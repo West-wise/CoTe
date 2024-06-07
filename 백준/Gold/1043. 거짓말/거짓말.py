9ca910913493587fe8cfbec7264b62108fd0f1be
PhoB <psh0036@suwon.ac.kr>
2024-06-07 17:30:43 +0900

    [Gold IV] Title: 거짓말, Time: 64 ms, Memory: 34028 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.py
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -3,11 +3,27 @@ from collections import deque
 
 input = sys.stdin.readline
 
+
+def BFS(parties, truth):
+
+    q = deque(truth)
+
+    while q:
+        person = q.popleft()
+        for party in parties:
+            if person in party:
+                for p in party:
+                    if p not in truth:
+                        q.append(p)
+                        truth.add(p)
+
+
 if __name__ == "__main__":
+
     truth = set()
 
     N, M = map(int, input().split())
-    answer = 0
+    answer = 0  # 거짓말을 말할 수 있는 파티 수,
     tmp = list(map(int, input().split()))
     truth.update(tmp[1:])
 
@@ -15,17 +31,7 @@ if __name__ == "__main__":
     for _ in range(M):
         parties.append(list(map(int, input().split()))[1:])
 
-    # 진실을 아는 사람들의 정보를 업데이트하기 위한 큐
-    queue = deque(truth)
-
-    while queue:
-        person = queue.popleft()
-        for party in parties:
-            if person in party:
-                for p in party:
-                    if p not in truth:
-                        truth.add(p)
-                        queue.append(p)
+    BFS(parties, truth)
 
     for party in parties:
         if all(person not in truth for person in party):
