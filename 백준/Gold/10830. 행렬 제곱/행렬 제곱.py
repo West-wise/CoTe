206d852e70207ba1dfe821990f9d949cdfb50d62
PhoB <psh0036@suwon.ac.kr>
2024-06-23 16:15:59 +0900

    [Gold IV] Title: 행렬 제곱, Time: 60 ms, Memory: 31836 KB -BaekjoonHub

--- 백준/Gold/10830. 행렬 제곱/행렬 제곱.py
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.py
@@ -15,22 +15,18 @@ def matrix_mul(matrix, rev):
 def matrix_pow(matrix, b):
     if b == 1:
         return [[elem % 1000 for elem in row] for row in matrix]
-    half = matrix_pow(matrix, b // 2)
-    half_squared = matrix_mul(half, half)
+
     if b % 2 == 0:
-        return half_squared
+        half = matrix_pow(matrix, b // 2)
+        return matrix_mul(half, half)
     else:
-        return matrix_mul(half_squared, matrix)
-
-
-def get_transposed_matrix(matrix, N):
-    return [[col[i] for col in matrix] for i in range(N)]
+        return matrix_mul(matrix, matrix_pow(matrix, b - 1))
 
 
 if __name__ == "__main__":
     N, B = map(int, input().split())
     matrix = [list(map(int, input().split())) for _ in range(N)]
-    flag = False if B % 2 == 0 else True
     result = matrix_pow(matrix, B)
+
     for row in result:
         print(" ".join(str(value) for value in row))
