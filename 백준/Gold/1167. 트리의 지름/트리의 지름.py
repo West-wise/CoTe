fad63d6b88ade070ecb1d3c48ec6307d283bd239
Park seohyeon <psh0036@suwon.ac.kr>
2024-11-01 02:55:42 +0900

    [Gold II] Title: 트리의 지름, Time: 620 ms, Memory: 98852 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -1,38 +1,38 @@
 import sys
-from collections import deque
+from collections import defaultdict
 
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
 
 def dfs(graph, start, visited):
     visited.add(start)
-    max_node, max_distance = start, 0
-    
-    for next_node, distance in graph[start]:
-        if next_node not in visited:
-            next_max_node, next_max_distance = dfs(graph, next_node, visited)
-            if next_max_distance + distance > max_distance:
-                max_distance = next_max_distance + distance
-                max_node = next_max_node
-                
-    return max_node, max_distance
+    dist_sum = 0
+    last_node = start
+    for node, distance in graph[start]:
+        if node not in visited:
+            next_node, total = dfs(graph, node, visited)
+            total += distance
+            if total > dist_sum:
+                dist_sum = total
+                last_node = next_node
+    return last_node, dist_sum
+
+
+def main():
+    graph = defaultdict(list)
+    # 1~V까지
+    V = int(input())  # 2 ≤ V ≤ 100,000
+    for i in range(1, V + 1):
+        arr = list(map(int, input().split()))
+        arr = arr[:-1]
+        key = arr.pop(0)
+        for k in range(0, len(arr), 2):
+            graph[key].append((arr[k], arr[k + 1]))
+
+    farthest, _ = dfs(graph, 1, set())
+    print(dfs(graph, farthest, set())[1])
+
 
 if __name__ == "__main__":
-    V = int(input())
-    graph = {i: [] for i in range(1, V + 1)}
-    
-    for _ in range(V):
-        data = list(map(int, input().split()))
-        node = data[0]
-        
-        # DEQUE 사용 부분 수정
-        edges = deque((data[i], data[i + 1]) for i in range(1, len(data) - 1, 2))
-        
-        while edges:
-            next_node, distance = edges.popleft()
-            graph[node].append((next_node, distance))
-            graph[next_node].append((node, distance))
-            
-    start_node, _ = dfs(graph, next(iter(graph.keys())), set())
-    _, result = dfs(graph, start_node, set())
-    
-    print(result)
+    main()
