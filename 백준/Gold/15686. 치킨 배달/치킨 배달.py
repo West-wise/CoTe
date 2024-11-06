971bb463c6f4daaf71b4b32bb6204943a297914d
Park seohyeon <psh0036@suwon.ac.kr>
2024-11-07 00:44:29 +0900

    [Gold V] Title: 치킨 배달, Time: 168 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.py
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.py
@@ -1,34 +1,21 @@
 import sys
 from itertools import combinations
 
-
 input = sys.stdin.readline
 
 def house_chicken(chicken, house):
-    result = []
+    total_distance = 0
     for r1, c1 in house:
-        min_distance = min(map(lambda chicken_pos: abs(r1 - chicken_pos[0]) + abs(c1 - chicken_pos[1]), chicken))
-        result.append(min_distance)
-
-    return sum(result)
-# def house_chicken(chicken, house):
-#     result = []
-#     for r1, c1 in house:
-#         tmp = []
-#         for r2, c2 in chicken:
-#             tmp.append(abs(r1-r2) + abs(c1-c2))
-#         result.append(min(tmp))
-#
-#     return sum(result)
-
-
-# 백 트래킹으로 지울 치킨집을 선택 or 그냥 브루트 포스로 진행..
-def remove_chicken(chicken, house):
-    pass
+        min_distance = float('inf')
+        for r2, c2 in chicken:
+            distance = abs(r1 - r2) + abs(c1 - c2)
+            if distance < min_distance:
+                min_distance = distance
+        total_distance += min_distance
+    return total_distance
 
 if __name__ == "__main__":
-    N,M = map(int,input().split())
-
+    N, M = map(int, input().split())
     city = [list(map(int, input().split())) for _ in range(N)]
 
     chicken = []
@@ -36,12 +23,15 @@ if __name__ == "__main__":
     for i in range(N):
         for k in range(N):
             if city[i][k] == 2:
-                chicken.append((i,k))
-            if city[i][k] == 1:
-                house.append((i,k))
+                chicken.append((i, k))
+            elif city[i][k] == 1:
+                house.append((i, k))
 
-    combi = list(combinations(chicken,M))
-    result = []
-    for combi_chicken in combi:
-        result.append(house_chicken(combi_chicken,house))
-    print(min(result))
+    # 가능한 치킨집 조합 중 최적의 결과만을 계산
+    min_total_distance = float('inf')
+    for combi_chicken in combinations(chicken, M):
+        total_distance = house_chicken(combi_chicken, house)
+        if total_distance < min_total_distance:
+            min_total_distance = total_distance
+
+    print(min_total_distance)
