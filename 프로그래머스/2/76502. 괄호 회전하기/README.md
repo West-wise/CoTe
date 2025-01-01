eaebade960cd7a735e403039bf98c507f76792c4
Park seohyeon <psh0036@suwon.ac.kr>
2025-01-02 00:26:19 +0900

    [level 2] Title: 괄호 회전하기, Time: 42.61 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502?language=java) 
 
 ### 성능 요약
 
-메모리: 76 MB, 시간: 0.30 ms
+메모리: 78.9 MB, 시간: 42.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 31일 21:04:14
+2025년 01월 02일 00:26:18
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,50 +1,37 @@
 import java.util.*;
 
+
 class Solution {
-    public int solution(String s) {
-        int answer = 0;
-        char[] arr = s.toCharArray();
-        ArrayList<Character> al = new ArrayList<>();
-        for(char c : arr){
-            al.add(c);
-        }
-        
-        for(int i=0; i<s.length(); i++){
-            if(check(al))answer++;;
-            al.add(al.get(0));//첫 요소를 받아와 맨 뒤에 추가
-            al.remove(0); //첫 요소 삭제;
-        }
-        return answer;
-    }
-    
-    private static boolean check(ArrayList al){
-        if(al.get(0).equals(')') || al.get(0).equals(']') || al.get(0).equals('}')) return false; //첫 시작이 닫는 괄호이면 틀림
-        
-        boolean answer = true;
-        Stack<Character>st = new Stack<>();
-        for(int i = 0; i <al.size(); i++){
-            char c = (Character)al.get(i);
-            if(c == '(' || c== '[' || c =='{') st.push(c);
-            else{
-                if(st.isEmpty()) return false;
-                switch(c){
-                case ')':
-                    if(st.peek().equals('(')) st.pop();
+    boolean check(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+            switch(c) {
+                case '(':
+                case '{':
+                case '[':
+                    stack.push(c);
                     break;
-                case ']': 
-                    if(st.peek().equals('[')) st.pop();
+                case ')':
+                    if(stack.isEmpty() || stack.pop() != '(') return false;
                     break;
                 case '}':
-                    if(st.peek().equals('{')) st.pop();
+                    if(stack.isEmpty() || stack.pop() != '{') return false;
                     break;
-                        
-            }
+                case ']':
+                    if(stack.isEmpty() || stack.pop() != '[') return false;
+                    break;
             }
         }
-        
-        if(st.size()!=0) answer =  false;
-        
+        return stack.isEmpty();
+    }
+    public int solution(String s) {
+        int answer = 0;
+        String tmp = s;
+        for(int i=0; i<s.length(); i++) {
+            if(check(tmp))answer++;
+            tmp = tmp.substring(1) + tmp.charAt(0);;
+        }
         return answer;
-
     }
 }
