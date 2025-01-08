75a647bfbe8fc22db027f2dc0843644356864686
Park seohyeon <psh0036@suwon.ac.kr>
2025-01-09 01:36:56 +0900

    [level 3] Title: 표 편집, Time: 186.15 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/README.md
+++ 프로그래머스/3/81303. 표 편집/README.md
@@ -1,10 +1,10 @@
 # [level 3] 표 편집 - 81303 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81303?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81303) 
 
 ### 성능 요약
 
-메모리: 114 MB, 시간: 110.08 ms
+메모리: 112 MB, 시간: 186.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 08일 00:46:35
+2025년 01월 09일 01:36:55
 
 ### 문제 설명
 
--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,46 +1,42 @@
 import java.util.*;
-
 class Solution {
-
     public String solution(int n, int k, String[] cmd) {
+        // 링크드 리스트와 스택..
         Stack<Integer> remove = new Stack<>();
-        int[] prev = new int[n];
-        int[] next = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            prev[i] = i - 1;
-            next[i] = i + 1;
+        int[] prev = new int[n+2];
+        int[] next = new int[n+2];
+
+        for(int i=0; i<n+2; i++){
+            prev[i] = i-1; // -1~8
+            next[i] = i+1; // 1~10
         }
-        next[n - 1] = -1; //마지막 노드는 next가 없다
-        int idx = k;
+        int idx = k+1;
 
-        for (String str : cmd) {
-            char command = str.charAt(0);
-            if (command == 'U') {
-                int num = Integer.parseInt(str.substring(2));
-                while (num-- > 0) {
-                    idx = prev[idx];
-                }
-            } else if (command == 'D') {
-                int num = Integer.parseInt(str.substring(2));
-                while (num-- > 0) {
-                    idx = next[idx];
-                }
-            } else if (command == 'C') {
+        for(String s : cmd){
+            if(s.startsWith("C")){ //현재 선택된 행을 삭제한 후, 바로 아래 행을 선택합니다. 단, 삭제된 행이 가장 마지막 행인 경우 바로 윗 행을 선택합니다
                 remove.push(idx);
-                if (prev[idx] != -1) next[prev[idx]] = next[idx];
-                if (next[idx] != -1) prev[next[idx]] = prev[idx];
+                prev[next[idx]] = prev[idx];
+                next[prev[idx]] = next[idx];
+                idx = next[idx] > n ? prev[idx] : next[idx];
+            }else if(s.startsWith("Z")){ //가장 최근에 삭제된 행을 원래대로 복구합니다. 단, 현재 선택된 행은 바뀌지 않습니다.
+                int num = remove.pop(); // 인덱스 복구
+                next[prev[num]] = num;
+                prev[next[num]] = num;
 
-                idx = (next[idx] != -1) ? next[idx] : prev[idx];
-            } else if (command == 'Z') {
-                int restore = remove.pop();
-                if (prev[restore] != -1) next[prev[restore]] = restore;
-                if (next[restore] != -1) prev[next[restore]] = restore;
+            } else {
+                String[] tmp = s.split(" ");
+                String command = tmp[0];
+                int num = Integer.parseInt(tmp[1]);
+                for(int i=0; i<num; i++){
+                    idx = command.equals("U") ? prev[idx] : next[idx];
+                }
             }
         }
-        StringBuilder result = new StringBuilder("O".repeat(n));
-        while (!remove.isEmpty()) {
-            result.setCharAt(remove.pop(), 'X');
+        StringBuilder result = new StringBuilder();
+        result.append("O".repeat(n));
+        for(int remove_index : remove){
+            result.setCharAt(remove_index-1,'X');
         }
         return result.toString();
     }
