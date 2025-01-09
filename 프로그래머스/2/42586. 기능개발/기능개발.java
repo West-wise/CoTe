439464293a22e36177a90d644d6ba5fb2af58a26
Park seohyeon <psh0036@suwon.ac.kr>
2025-01-09 13:30:33 +0900

    [level 2] Title: 기능개발, Time: 5.11 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,26 +1,27 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        ArrayList<Integer> res = new ArrayList<>();
+        ArrayList<Integer> answer = new ArrayList<>();
         int[] dday = new int[progresses.length];
+
         for(int i=0; i<progresses.length; i++) {
             dday[i] = (int)Math.ceil((100.0-progresses[i])/speeds[i]);
         }
-        Arrays.stream(dday).forEach(queue::add);
 
-        int cnt =0;
+        int cnt = 0;
         int MaxDay = dday[0];
+
         for(int i=0; i<progresses.length; i++) {
-            if(dday[i] <= MaxDay){
+            if(dday[i] <= MaxDay) {
                 cnt++;
-            } else {
-                res.add(cnt);
-                cnt = 1;
+            } else{
+                answer.add(cnt);
                 MaxDay = dday[i];
+                cnt = 1;
             }
         }
-        res.add(cnt);
-        return res.stream().mapToInt(Integer::valueOf).toArray();
+        answer.add(cnt);
+
+        return answer.stream().mapToInt(i->i).toArray();
     }
 }
