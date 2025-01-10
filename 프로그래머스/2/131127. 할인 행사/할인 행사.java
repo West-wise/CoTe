2c7a6d39b78ee1624df35864f0104374c983d825
Park seohyeon <psh0036@suwon.ac.kr>
2025-01-11 01:33:18 +0900

    [level 2] Title: 할인 행사, Time: 47.87 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,28 +1,46 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String[] want, int[] number, String[] discount) {
-        int answer = 0,sum=0 ,cnt =0;
-        Map<String,Integer> hm = new HashMap<>() , solution = new HashMap<>();
-        List<String> al = new ArrayList<>(); 
-       
+    public boolean windowCheck(Map<String, Integer> map, String[] want, int[] number){
+        for(String goods : want){ // 원하는 상품이 없다면?
+            if(!map.containsKey(goods))  return false;
+        }
+
+        //원하는 상품은 다 있는데 갯수가 부족하다면?
         for(int i=0; i<want.length; i++){
-            solution.put(want[i],number[i]);
+            if(map.get(want[i]) != number[i]) return false;
         }
-        
-        for(int k=0; k<=discount.length-10; k++){
-            al.clear();
-            hm.clear();
-            for(String s : want){ hm.put(s,0); }
-            for(int i=k; i<k+10; i++){ al.add(discount[i]);}//10개만 넣기
-            for(String str : al){
-                if(hm.containsKey(str)){ hm.put(str,hm.get(str)+1); }
-                else break;
-            }
-            if(solution.equals(hm)){
-                cnt++;
-            }
+
+        return true;
+    }
+    public int solution(String[] want, int[] number, String[] discount) {
+        Map<String, Integer> map = new HashMap<>() , window = new HashMap<>();
+        int answer = 0;
+
+        for(int i=0; i<want.length; i++) {
+            map.put(want[i],number[i]);
         }
-        return cnt;
+        // 첫번째 윈도우
+        for(int i=0; i<10; i++){
+            window.put(discount[i], window.getOrDefault(discount[i], 0) + 1);
+        }
+        if(windowCheck(window, want, number)) answer++;
+
+
+        for(int i = 10; i<discount.length; i++) {
+            // 오래된 부분 제거
+            String remove = discount[i-10];
+            window.put(remove, window.get(remove) - 1);
+            if(window.get(remove) == 0){
+                window.remove(remove);
+            }
+            String newItem = discount[i];
+            window.put(newItem, window.getOrDefault(newItem, 0) + 1);
+            if (windowCheck(window, want, number)) answer++;
+
+        }
+
+
+        return answer;
     }
 }
