fa811c77381a421e144fa8d885803fba599e1d91
Park seohyeon <psh0036@suwon.ac.kr>
2025-01-15 01:24:38 +0900

    [level 2] Title: 예상 대진표, Time: 0.06 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/README.md
+++ 프로그래머스/2/12985. 예상 대진표/README.md
@@ -1,10 +1,10 @@
 # [level 2] 예상 대진표 - 12985 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12985?language=java) 
 
 ### 성능 요약
 
-메모리: 77.6 MB, 시간: 0.15 ms
+메모리: 88.1 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 31일 21:02:55
+2025년 01월 15일 01:24:36
 
 ### 문제 설명
 
--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,23 +2,15 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        //단, A번 참가자와 B번 참가자는 서로 붙게 되기 전까지 항상 이긴다고 가정합니다.
-        int answer = 1; //1라운드부터 시작
- 
-        while(Math.abs(a - b) != 1 || a / 2 == b / 2){
-            a = div(a);
-            b = div(b);
+        // 항상 이긴다고 가정
+        // 부전승 없음
+        int answer = 1;
+
+        while(Math.abs(a-b) != 1 || a/2 == b/2){
+            a = a%2 == 0 ? a/2 : a/2+1;
+            b = b%2 == 0 ? b/2 : b/2+1;
             answer++;
         }
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println(b);
-
         return answer;
-    }
-    
-    private static int div(int num){
-        if(num==1)return num;
-        return (num%2==0) ? num/2 : num/2+1;
     }
 }
