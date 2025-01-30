a1bd868697bf46ce9726110647e41cfb354d1b4d
Park seohyeon <psh0036@suwon.ac.kr>
2025-01-30 21:49:44 +0900

    [level 3] Title: 네트워크, Time: 1.99 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,40 +1,34 @@
 import java.util.*;
 
 class Solution {
-    public int DFS(Map<Integer, List<Integer>> graph, int start, Set<Integer> visited) {
+
+    public void DFS(Map<Integer, List<Integer>> graph, int start, Set<Integer> visited){
         visited.add(start);
-        int size = 1; // 현재 노드를 포함한 네트워크 그룹의 크기
-        for (int neighbor : graph.get(start)) {
-            if (!visited.contains(neighbor)) {
-                size += DFS(graph, neighbor, visited);
+        for(int neighbor : graph.get(start)){
+            if(!visited.contains(neighbor)){
+              DFS(graph, neighbor, visited);
             }
         }
-        return size;
     }
-
     public int solution(int n, int[][] computers) {
-        Map<Integer, List<Integer>> graph = new HashMap<>();
-
-        // 그래프 생성
-        for (int i = 0; i < computers.length; i++) {
-            for (int j = 0; j < computers[i].length; j++) {
-                if (computers[i][j] == 1) {
-                    graph.computeIfAbsent(i, k -> new ArrayList<>()).add(j);
-                }
-            }
-        }
-
         int answer = 0;
-        Set<Integer> visited = new HashSet<>();
-
-        // 모든 노드를 탐색하며 네트워크 그룹의 개수를 센다
-        for (int i = 0; i < computers.length; i++) {
-            if (!visited.contains(i)) {
-                answer += 1;
-                DFS(graph, i, visited);
+        // 1.일단 그래프를 만들어
+        Map<Integer,List<Integer>> graph = new HashMap<>();
+        for(int i=0; i<computers.length; i++){
+            for(int k=0; k<computers[i].length; k++){
+                if(computers[i][k] == 1 ) graph.computeIfAbsent(i,j->new ArrayList<>()).add(k);
             }
         }
 
+        // DFS를 통해 탐색하다 보면 연결된 컴퓨터가 나온다.
+        // 이미 탐색한 것을 막기위해 set 생성
+        Set<Integer> visited = new HashSet<>();
+        for(int key : graph.keySet()){
+            if(!visited.contains(key)){
+                DFS(graph, key, visited);
+                answer++;
+            }
+        }
         return answer;
     }
 }
