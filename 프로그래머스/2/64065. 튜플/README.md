20f7897ec887f0acbe1c320feb0d2adf7412d13d
Park seohyeon <psh0036@suwon.ac.kr>
2025-02-07 20:07:21 +0900

    [level 2] Title: 튜플, Time: 99.08 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/README.md
+++ 프로그래머스/2/64065. 튜플/README.md
@@ -1,10 +1,10 @@
 # [level 2] 튜플 - 64065 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64065?language=java) 
 
 ### 성능 요약
 
-메모리: 79.4 MB, 시간: 0.35 ms
+메모리: 110 MB, 시간: 99.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 31일 21:07:07
+2025년 02월 07일 20:07:16
 
 ### 문제 설명
 
--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,37 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        
-        StringBuilder sb = new StringBuilder(s);
-        //1. 특수문자 제거
-        s = s.replaceAll("[^ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9]"," ");//특수문자를 공백으로 치환
-        
-        //2. 공백 을 기준으로 split
-        String[] arr = s.split(" ");
-        StringTokenizer st = new StringTokenizer(s," ",false);
-        
-        //3. HashMap에 삽입
-        Map<String,Integer> hm = new HashMap<>();
-        while(st.hasMoreTokens()){
-            String key = st.nextToken();
-            if(hm.containsKey(key)) hm.put(key,hm.get(key)+1);
-            else hm.put(key,1);
-        }
-        
-        int[] answer = new int[hm.size()];
-        int cnt = hm.size();
-        int idx =0;
-        //4. value값이 높은순서대로 key반환
-        for(int i=0; i<answer.length; i++){
-            for(String key : hm.keySet()){
-                int value = hm.get(key);
-                if(cnt == value){
-                    cnt--;
-                    answer[i] = Integer.parseInt(key);
-                    break;
+
+        List<List<String>> current = new ArrayList<>();
+        String[] split_list = s.split("[{},]");
+        List<String> tmp = new ArrayList<>();
+        for(String part : split_list) {
+            if(part.length() != 0){
+                tmp.add(part);
+            } else{
+                if(!tmp.isEmpty()){
+                    current.add(new ArrayList<>(tmp));
+                    tmp = new ArrayList<>();
                 }
             }
         }
-        return answer;
+        current.add(new ArrayList<>(tmp));
+        current.sort(Comparator.comparingInt(List::size));
+
+
+        LinkedHashSet<String> set  = new LinkedHashSet<>();
+        for(List<String> arr : current){
+            set.addAll(arr);
+        }
+
+        return set.stream().mapToInt(Integer::valueOf).toArray();
     }
 }
