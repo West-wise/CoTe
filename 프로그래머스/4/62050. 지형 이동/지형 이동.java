4e6f98575ad8540ffc9a9d9642fe068072216e71
Park seohyeon <psh0036@suwon.ac.kr>
2025-02-10 18:21:43 +0900

    [level 4] Title: 지형 이동, Time: 131.67 ms, Memory: 97.3 MB -BaekjoonHub

--- 프로그래머스/4/62050. 지형 이동/지형 이동.java
+++ 프로그래머스/4/62050. 지형 이동/지형 이동.java
@@ -1,30 +1,27 @@
 import java.util.*;
 class Solution {
     private static class Node{
-        public int x;
-        public int y;
-        public int cost;
+        int x,y,cost;
         public Node(int x, int y, int cost) {
             this.x = x;
             this.y = y;
             this.cost = cost;
         }
     }
-
-    private static int getAns(int[][] land, int height){
+    public int solution(int[][] land, int height) {
+        int answer = 0;
         int rows = land.length, cols = land[0].length;
         int[][] directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
         boolean[][] visited = new boolean[rows][cols];
 
-        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(node -> node.cost));
-        PriorityQueue<int[]> ladder = new PriorityQueue<>(Comparator.comparingInt(arr -> arr[2]));
+        PriorityQueue<int[]> ladder = new PriorityQueue<>(Comparator.comparingInt(array -> array[2]));
+        PriorityQueue<Node> nodeQ = new PriorityQueue<>(Comparator.comparingInt(node -> node.cost));
 
+        nodeQ.add(new Node(0, 0, 0));
         visited[0][0] = true;
-        pq.add(new Node(0,0, 0));
-        int ladderCnt = 0;
 
-        while(!pq.isEmpty()){
-            Node node = pq.poll();
+        while(!nodeQ.isEmpty()){
+            Node node = nodeQ.poll();
             int x = node.x, y = node.y, cost = node.cost;
             for(int[] dir : directions){
                 int nx = x + dir[0], ny = y + dir[1];
@@ -32,26 +29,23 @@ class Solution {
                     int diff = Math.abs(land[x][y] - land[nx][ny]);
                     if(diff <= height){
                         visited[nx][ny] = true;
-                        pq.add(new Node(nx, ny, cost));
+                        nodeQ.add(new Node(nx, ny, cost));
                     } else {
                         ladder.add(new int[]{nx, ny, diff});
                     }
                 }
             }
-            while(!pq.isEmpty() || !ladder.isEmpty()){
-                if(!pq.isEmpty()) break;
-                int[] next = ladder.poll();
-                int nx = next[0], ny = next[1], diff = next[2];
+
+            while(!ladder.isEmpty() || !nodeQ.isEmpty()){
+                if(!nodeQ.isEmpty()) break;
+                int[] tmp = ladder.poll();
+                int nx = tmp[0], ny = tmp[1], diff = tmp[2];
                 if(visited[nx][ny]) continue;
-                ladderCnt += diff;
                 visited[nx][ny] = true;
-                pq.add(new Node(nx, ny, 0));
+                answer += diff;
+                nodeQ.add(new Node(nx, ny, 0));
             }
         }
-        return ladderCnt;
-    }
-
-    public int solution(int[][] land, int height) {
-        return getAns(land, height);
+        return answer;
     }
 }
