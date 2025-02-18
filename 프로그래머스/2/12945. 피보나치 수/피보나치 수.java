0be017e01eea748607a3c7927cf613d12646a3f5
Park seohyeon <psh0036@suwon.ac.kr>
2025-02-18 20:09:44 +0900

    [level 2] Title: 피보나치 수, Time: 2.40 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,14 +1,11 @@
 import java.util.*;
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        ArrayList<Integer> al = new ArrayList<>();
-        for(int i=0; i<=n;i++){
-            if(i<=1) al.add(i);
-            else {
-                al.add((al.get(i-1)+al.get(i-2))%1234567);
-            }
+        int[] fibo = new int[n + 1];
+        fibo[1] = 1;
+        for(int i = 2; i <= n; i++){
+            fibo[i] = (fibo[i-1] + fibo[i-2])% 1234567;
         }
-        return al.get(n);
+        return fibo[n] ;
     }
 }
