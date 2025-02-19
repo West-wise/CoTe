337ac212931c88ab6399ea3000dfed93aafff430
Park seohyeon <psh0036@suwon.ac.kr>
2025-02-19 20:49:24 +0900

    [level 2] Title: 땅따먹기, Time: 14.73 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,18 +1,16 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] land) {
-        int[][] dp = new int[land.length][];
-        for (int i = 0; i < land.length; i++) {
-            dp[i] = Arrays.copyOf(land[i], land[i].length);
-        }
-        for(int i = 0; i < land.length-1; i++){
+        for(int i = 1; i < land.length; i++){
             for(int k = 0; k < land[i].length; k++){
+                int maxPrev = 0;
                 for(int j = 0; j < land[i].length; j++){
                     if(k == j) continue;
-                    dp[i+1][j] = Math.max(dp[i+1][j],dp[i][k] + land[i+1][j]);
+                    maxPrev = Math.max(maxPrev, land[i-1][j]);
                 }
+                land[i][k] += maxPrev;
             }
         }
-        return Arrays.stream(dp[dp.length-1]).max().getAsInt();
+        return Arrays.stream(land[land.length-1]).max().getAsInt();
     }
 }
