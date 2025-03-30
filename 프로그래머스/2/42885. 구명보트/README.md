98e095202ba87d82d29f965635d28d3a816007d8
Park seohyeon <psh0036@suwon.ac.kr>
2025-03-30 23:32:22 +0900

    [level 2] Title: 구명보트, Time: 37.31 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/README.md
+++ 프로그래머스/2/42885. 구명보트/README.md
@@ -1,10 +1,10 @@
 # [level 2] 구명보트 - 42885 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42885?language=java) 
 
 ### 성능 요약
 
-메모리: 55.9 MB, 시간: 21.77 ms
+메모리: 73.9 MB, 시간: 37.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 31일 21:03:13
+2025년 03월 30일 23:32:20
 
 ### 문제 설명
 
--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -3,14 +3,25 @@ import java.util.*;
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
-       
-        Arrays.sort(people); //오름차순 정렬
-        int min = 0;
-        for(int max = people.length-1; max>=min; max--){
-            if(people[max]+people[min]<=limit)min++;
+        // 최대 2명만 태울수 있다.
+
+        Arrays.sort(people);  // 오름차순 정렬
+        Deque<Integer> dq = new LinkedList<>();
+        for (int num : people) {
+            dq.add(num);
+        }
+        while(dq.size()>=2){
+            int left=dq.peek();
+            int right=dq.getLast();
+            if(left+right <= limit){
+                dq.removeFirst();
+            }
+            dq.removeLast();
             answer++;
         }
-        
+        if(!dq.isEmpty()){
+            answer++;
+        }
         return answer;
     }
 }
