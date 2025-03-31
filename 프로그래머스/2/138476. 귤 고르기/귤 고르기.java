6f1880f6fa2b82b691e75c30f33db63eabcc4229
Park seohyeon <psh0036@suwon.ac.kr>
2025-03-31 17:13:01 +0900

    [level 2] Title: 귤 고르기, Time: 84.05 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -1,27 +1,20 @@
 import java.util.*;
 
-
 class Solution {
     public int solution(int k, int[] tangerine) {
-        int answer = 0;
-        
+        int answer = 1;
         Map<Integer,Integer> map = new HashMap<>();
-        for(int i : tangerine){
-            map.put(i,map.getOrDefault(i,0)+1);
+
+        for(int size : tangerine){
+            map.put(size,map.computeIfAbsent(size,x -> 0) + 1);
         }
-        List<Integer> list = new ArrayList<>();
-        
-        int sum = 0;
-        for(Integer i : map.keySet()){
-            list.add(map.get(i));
-        }
-        
-        list.sort(Collections.reverseOrder());
-        
-        for(Integer i : list){
+        List<Integer> keyList = new ArrayList<>(map.keySet());
+        keyList.sort((a,b) -> map.get(b) - map.get(a));
+
+        for(int num : keyList){
+            k -= map.get(num);
+            if(k<=0) break;
             answer++;
-            sum += i;
-            if(sum>=k)break;
         }
         return answer;
     }
