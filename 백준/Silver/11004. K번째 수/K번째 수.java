46f61b7b87bacbc8b81a0b4048fe7bbbee6efc9e
Park seohyeon <psh0036@suwon.ac.kr>
2025-04-30 01:35:17 +0900

    [Silver V] Title: K번째 수, Time: 3292 ms, Memory: 560284 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -3,14 +3,25 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        String[] line = input.readLine().split(" ");
-        int n = Integer.parseInt(line[0]);
-        int m = Integer.parseInt(line[1]);
-        String[] inputArr = input.readLine().split(" ");
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int i = 0; i < n; i++) pq.add(Integer.parseInt(inputArr[i]));
-        for(int i = 0; i<m-1; i++) pq.poll();
-        System.out.println(pq.poll());
+        // 입력을 빠르게 받기 위한 BufferedReader 사용
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        // 숫자들을 저장할 ArrayList 생성
+        List<Integer> list = new ArrayList<>(N);
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        // Collections.sort()로 정렬
+        Collections.sort(list);
+
+        // K번째 수 출력 (인덱스는 0부터 시작하므로 K-1)
+        System.out.println(list.get(K - 1));
     }
 }
