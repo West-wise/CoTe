891f6b796fba81cdf274c9af509976eda34baea0
Park seohyeon <psh0036@suwon.ac.kr>
2025-05-06 01:35:34 +0900

    [Gold V] Title: ABCDE, Time: 452 ms, Memory: 148632 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -3,28 +3,26 @@ import java.util.*;
 
 public class Main { ;
     private static Map<Integer, List<Integer>> friends;
-    private static int n,m;
-    private static boolean dfs(int node, Set<Integer> visited, int depth) {
+    private static boolean dfs(int node, boolean[] visited, int depth) {
         if(depth == 5) return true;
-        visited.add(node);
+        visited[node] = true;
         if(friends.get(node) != null){
             for(int val : friends.get(node)){
-                if(!visited.contains(val)){
+                if(!visited[val]){
                     if(dfs(val, visited, depth+1)) return true;
                 }
             }
         }
-        visited.remove(node);
+        visited[node] = false;
         return false;
     }
     public static void main(String[] args) throws IOException {
         // 입력을 빠르게 받기 위한 BufferedReader 사용
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(input.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
         friends = new HashMap<>();
-
         for(int i = 0; i < m; i++){
             st = new StringTokenizer(input.readLine());
             int a = Integer.parseInt(st.nextToken());
@@ -33,7 +31,7 @@ public class Main { ;
             friends.computeIfAbsent(b, k -> new ArrayList<>()).add(a);
         }
         for(int  i=0; i<n; i++){
-            if(dfs(i, new HashSet<>(), 1)){
+            if(dfs(i, new boolean[n], 1)){
                 System.out.println(1);
                 return;
             }
