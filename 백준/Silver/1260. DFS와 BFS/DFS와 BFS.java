3ce7c616999b26b8c913678829d0d02c435aa1b7
Park seohyeon <psh0036@suwon.ac.kr>
2025-05-06 16:44:22 +0900

    [Silver II] Title: DFS와 BFS, Time: 276 ms, Memory: 22012 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,62 +1,66 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        HashMap<Integer,ArrayList<Integer>> graph = new HashMap<>();
-
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int V = sc.nextInt();
-
-        ArrayList<Integer> visited1 = new ArrayList<>();
-        ArrayList<Integer> visited2 = new ArrayList<>();
-
-        for(int i =1; i<=N;i++){
-            graph.put(i,new ArrayList<Integer>());
-        }
-
-        for(int i=0; i<M; i++){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-
-            graph.get(a).add(b);
-            graph.get(b).add(a);
-        }
-
-        for(int key : graph.keySet()){
-            Collections.sort(graph.get(key));
-        }
-
-        dfs(graph,V,visited1);
-        System.out.println();
-        bfs(graph,V,visited2);
-    }
-
-    static void dfs(HashMap<Integer, ArrayList<Integer>> graph, int start, ArrayList<Integer> visited) {
-        System.out.print(start + " ");
-        visited.add(start);
-        for (int key : graph.get(start)) {
-            if (!visited.contains(key)) {
-                dfs(graph, key, visited);
+    public static StringBuilder dfsList = new StringBuilder();
+    public static StringBuilder bfsList = new StringBuilder();
+    public static void dfs(int node, Map<Integer,List<Integer>> map, boolean[] visited){
+        visited[node] = true;
+        dfsList.append(node).append(" ");
+        List<Integer> list = map.get(node);
+        if(list != null){
+            for(int i : list){
+                if(!visited[i]) dfs(i, map, visited);
             }
         }
     }
 
-    static void bfs(HashMap<Integer, ArrayList<Integer>> graph, int start, ArrayList<Integer> visited){
-        System.out.printf(start + " ");
-        visited.add(start);
+    public static void bfs(int node, Map<Integer, List<Integer>> map, boolean[] visited){
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
+        visited[node] = true;
+        queue.add(node);
 
-        while (!queue.isEmpty()) {
-            for (int node : graph.get(queue.poll())) {
-                if (!visited.contains(node)) {
-                    System.out.printf(node + " ");
-                    visited.add(node);
-                    queue.add(node);
+        while(!queue.isEmpty()){
+            int current = queue.poll();
+            bfsList.append(current).append(" ");
+            List<Integer> list = map.get(current);
+            if(list != null){
+                for(int neighbor : list){
+                    if(!visited[neighbor]){
+                        queue.add(neighbor);
+                        visited[neighbor] = true;
+                    }
                 }
             }
         }
+    }
+    public static void main(String[] args) throws IOException {
+        // 입력을 빠르게 받기 위한 BufferedReader 사용
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(input.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int v = Integer.parseInt(st.nextToken());
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for(int i = 0; i<m; i++){
+            st = new StringTokenizer(input.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            map.computeIfAbsent(a, k -> new ArrayList<>()).add(b);
+            map.computeIfAbsent(b, k -> new ArrayList<>()).add(a);
+        }
+
+        for(int node : map.keySet()){
+            Collections.sort(map.get(node));
+        }
+
+        boolean[] visited = new boolean[n+1];
+        dfs(v, map, visited);
+        visited = new boolean[n+1];
+        bfs(v, map, visited);
+
+        System.out.println(dfsList.toString());
+        System.out.println(bfsList.toString());
+
     }
 }
