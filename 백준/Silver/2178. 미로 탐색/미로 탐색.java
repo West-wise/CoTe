4e73cd79cc7a3f285dbd3e93e4fcf442118f0846
Park seohyeon <psh0036@suwon.ac.kr>
2025-05-06 17:55:42 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 12064 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -4,22 +4,20 @@ import java.util.*;
 public class Main {
     public static int N, M;
     public static int bfs(int startX, int startY, int[][] board, boolean[][] visited){
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{startX, startY, 0});
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{startX, startY, 1});
         visited[startX][startY] = true;
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
         while(!queue.isEmpty()){
             int[] current = queue.poll();
             int x = current[0],y = current[1];
-            if(x == N-1 && y == M-1) return current[2]+1;
+            if(x == N-1 && y == M-1) return current[2];
             for(int i = 0; i<4; i++){
                 int nx = x + dx[i], ny = y + dy[i];
-                if(nx >= 0 && nx < N && ny >= 0 && ny < M && board[nx][ny] == 1){
-                    if(!visited[nx][ny]){
-                        visited[nx][ny] = true;
-                        queue.add(new int[]{nx, ny, current[2] + 1});
-                    }
+                if(nx >= 0 && nx < N && ny >= 0 && ny < M && board[nx][ny] == 1 && !visited[nx][ny]){
+                    visited[nx][ny] = true;
+                    queue.add(new int[]{nx, ny, current[2] + 1});
                 }
             }
         }
@@ -35,7 +33,7 @@ public class Main {
         for (int i = 0; i < N; i++) {
             String tmp = input.readLine();
             for(int k = 0; k<M; k++){
-                matrix[i][k] = Integer.parseInt(tmp.charAt(k)+"");
+                matrix[i][k] = tmp.charAt(k) - '0';
             }
         }
         System.out.println(bfs(0,0,matrix, new boolean[N][M]));
