fb64c1612a658a2be93d87178bf598ece777bd18
Park seohyeon <psh0036@suwon.ac.kr>
2025-05-09 17:55:29 +0900

    [Gold II] Title: 트리의 지름, Time: 720 ms, Memory: 109320 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -2,41 +2,76 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int N;
-    public static int[] dfs(int start, Map<Integer, List<int[]>> map, boolean[] visited){
-        int dist_sum = 0;
-        int last_node = start;
+    static int N;
+
+    // 간선 클래스 정의
+    static class Edge {
+        int to, cost;
+        Edge(int to, int cost) {
+            this.to = to;
+            this.cost = cost;
+        }
+    }
+
+    // DFS 결과 저장용 클래스
+    static class Result {
+        int node, dist;
+        Result(int node, int dist) {
+            this.node = node;
+            this.dist = dist;
+        }
+    }
+
+    public static Result dfs(int start, List<Edge>[] map, boolean[] visited) {
+        if (visited == null || map == null) {
+            throw new IllegalArgumentException("입력값이 null입니다.");
+        }
+
         visited[start] = true;
-        List<int[]> neighbors = map.get(start);
-        if(neighbors != null){
-            for(int[] neighbor : neighbors){
-                if(!visited[neighbor[0]]){
-                    int[] next = dfs(neighbor[0], map, visited);
-                    int tmp_cost = next[1] + neighbor[1];
-                    if(tmp_cost > dist_sum){
-                        dist_sum = tmp_cost;
-                        last_node = next[0];
+        int maxDist = 0;
+        int farthestNode = start;
+
+        if (map[start] != null) {
+            for (Edge edge : map[start]) {
+                if (edge != null && !visited[edge.to]) {
+                    Result res = dfs(edge.to, map, visited);
+                    int totalDist = res.dist + edge.cost;
+                    if (totalDist > maxDist) {
+                        maxDist = totalDist;
+                        farthestNode = res.node;
                     }
                 }
             }
         }
-        return new int[]{last_node, dist_sum};
+
+        return new Result(farthestNode, maxDist);
     }
+
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) throws IOException {
-        // 입력을 빠르게 받기 위한 BufferedReader 사용
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(input.readLine());
-        Map<Integer, List<int[]>> map = new HashMap<>();
-        for(int i = 0; i<N; i++){
-            String[] data = input.readLine().split(" ");
-            int start = Integer.parseInt(data[0]);
-            for(int k = 1; k < data.length-1; k+=2){
-                int end = Integer.parseInt(data[k]);
-                int cost = Integer.parseInt(data[k+1]);
-                map.computeIfAbsent(start, arr -> new ArrayList<>()).add(new int[]{end, cost});
+
+        // 배열로 바꿔 성능 향상
+        List<Edge>[] map = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            map[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(input.readLine());
+            int start = Integer.parseInt(st.nextToken());
+
+            while (st.hasMoreTokens()) {
+                int end = Integer.parseInt(st.nextToken());
+                if (end == -1) break; // 문제 정의에 따라 종료
+                int cost = Integer.parseInt(st.nextToken());
+                map[start].add(new Edge(end, cost));
             }
         }
-        int[] first = dfs(1, map, new boolean[N+1]);
-        System.out.println(dfs(first[0], map, new boolean[N + 1])[1]);
+
+        Result first = dfs(1, map, new boolean[N + 1]);
+        Result second = dfs(first.node, map, new boolean[N + 1]);
+        System.out.println(second.dist);
     }
 }
