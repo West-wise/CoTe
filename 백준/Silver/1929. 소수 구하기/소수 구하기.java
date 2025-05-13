3ed3d0b066474fc8021d050475e7545ae4709eb8
Park seohyeon <psh0036@suwon.ac.kr>
2025-05-13 11:27:21 +0900

    [Silver III] Title: 소수 구하기, Time: 176 ms, Memory: 22388 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -11,22 +11,23 @@ public class Main {
         StringTokenizer st = new StringTokenizer(input.readLine());
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N+1];
-        for(int i = M; i<=N; i++){
-            arr[i] = 1;
-        }
-        arr[1] = 0;
-        for(int i = 2; i<=Math.sqrt(N); i++){
-            for(int k = 2; k <= N/i; k++){
-                arr[i*k] = 0;
+        boolean[] arr = new boolean[N+1];
+        arr[1] = true;
+        int sqrt = (int) Math.sqrt(N);
+        for(int i = 2; i<=sqrt; i++){
+            if(!arr[i]){
+                for(int k = i*i; k <= N; k+=i){
+                    arr[k] = true;
+                }
             }
         }
+        BufferedWriter bf = new BufferedWriter(new OutputStreamWriter(System.out));
         for(int i = M; i<=N; i++){
-            if(arr[i] != 0){
-                System.out.println(i);
+            if(!arr[i]){
+                bf.write(i + "\n");
             }
         }
+        bf.flush();
+        bf.close();
     }
-
-
 }
