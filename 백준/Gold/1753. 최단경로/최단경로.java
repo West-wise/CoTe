52bda85c0a59b449f384bb4a220c6b1b211783de
Park seohyeon <psh0036@suwon.ac.kr>
2025-06-20 01:04:28 +0900

    [Gold IV] Title: 최단경로, Time: 888 ms, Memory: 127472 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -26,46 +26,44 @@ public class Main {
             map.get(from).add(new int[]{to, cost});
         }
 
-        Map<Integer, Integer> res = Dijkstra(start);
+        int[] res = Dijkstra(start);
 
         StringBuilder sb = new StringBuilder();
         for(int i = 1; i<=N; i++){
-            if(res.get(i) == Integer.MAX_VALUE){
+            if(res[i] == Integer.MAX_VALUE){
                 sb.append("INF\n");
-            } else if(res.get(i) == 0){
+            } else if(res[i] == 0){
                 sb.append("0\n");
             } else{
-                sb.append(res.get(i)).append("\n");
+                sb.append(res[i]).append("\n");
             }
         }
-        System.out.println(sb.toString());
+        System.out.println(sb);
 
     }
 
-    public static Map<Integer, Integer> Dijkstra(int start){
-        Map<Integer, Integer> distance = new HashMap<>();
-        for(int i =1; i<=N; i++){
-            distance.put(i, Integer.MAX_VALUE); // 가중치 최대값이 10이기 때문에 그냥 11로 퉁치자
+    public static int[] Dijkstra(int start){
+        int[] dist = new int[N+1]; // 인덱스가 노드, 값이 코스트
+        for(int i = 1; i <= N; i++){
+            dist[i] = Integer.MAX_VALUE;
         }
-        distance.put(start, 0); // 시작 노드만 0으로 변경
+        dist[start] = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b) -> a[0] - b[0]);
+        pq.add(new int[]{0, start});
 
-        PriorityQueue<int[]> queue = new PriorityQueue<>((a,b) -> Integer.compare(a[0],b[0]));
-        queue.add(new int[]{0, start});
-
-        while(!queue.isEmpty()){
-            int[] current = queue.poll();
+        while(!pq.isEmpty()){
+            int[] current = pq.poll();
             int current_weight = current[0], current_node = current[1];
-            if(current_weight > distance.get(current_node)) continue;
-            for(int[] tuple : map.get(current_node)){
-                int weight = tuple[1], node = tuple[0];
-                int new_dist = weight + current_weight;
-                if(new_dist < distance.get(node)){
-                    distance.put(node, new_dist);
-                    queue.add(new int[]{new_dist, node});
+            if(current_weight < dist[current_node]) continue;
+            for(int[] node_list : map.get(current_node)){
+                int weight = node_list[1], new_node = node_list[0];
+                int new_weight = weight + current_weight;
+                if(new_weight < dist[new_node]){
+                    dist[new_node] = new_weight;
+                    pq.add(new int[]{new_weight, new_node});
                 }
             }
-
         }
-        return distance;
+        return dist;
     }
 }
