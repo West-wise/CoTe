31a2fbb4aea954e90d631a632a2a31da6ed6085b
Park seohyeon <psh0036@suwon.ac.kr>
2025-10-05 00:14:17 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 4.64 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        for(int i = 1; i<n; i++){
+            if(n % i == 1) return i;
+        }
+        return 0;
+    }
+}
