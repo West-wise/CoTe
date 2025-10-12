df611a90d654b1287e300c056444dfa03e7bb527
Park seohyeon <psh0036@suwon.ac.kr>
2025-10-12 22:18:47 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.66 ms, Memory: 56 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,28 +1,26 @@
 import java.util.*;
 class Solution {
-    public int solution(int[][] maps) {
-        // 가장 빠른 경로를 찾아야 하므로 BFS가 아닐까 싶다
+    private int bfs(int[][] maps, boolean[][] visited, int[] start){
         int rows = maps.length, cols = maps[0].length;
-        boolean[][] visited = new boolean[rows][cols];
         int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        Queue<int[]> queue = new ArrayDeque<>();
-        visited[0][0] = true;
-        queue.add(new int[]{0, 0, 1});
-
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(start);
         while(!queue.isEmpty()){
             int[] current = queue.poll();
             int x = current[0], y = current[1], cost = current[2];
-            if(x == rows - 1 && y == cols -1) return cost;
+            if(x == rows-1 && y == cols -1) return cost;
             for(int[] dir : directions){
                 int nx = x + dir[0], ny = y + dir[1];
-                if(nx >= 0 && nx < rows && ny >= 0 && ny < cols && !visited[nx][ny] && maps[nx][ny] != 0){
+                if(nx >= 0 && nx < rows && ny>=0 && ny < cols && !visited[nx][ny] && maps[nx][ny] != 0){
                     visited[nx][ny] = true;
-                    queue.add(new int[]{nx, ny, cost + 1});
+                    queue.add(new int[]{nx, ny, cost+1});
                 }
             }
         }
-
-
         return -1;
+    }
+    public int solution(int[][] maps) {
+        boolean[][] visited = new boolean[maps.length][maps[0].length];
+        return bfs(maps, visited, new int[]{0,0,1});
     }
 }
