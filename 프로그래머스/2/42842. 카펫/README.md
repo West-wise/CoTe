98004f900fbd11909d3db6c6b00c7eed40b2b946
Park seohyeon <psh0036@suwon.ac.kr>
2025-10-12 18:50:47 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
 
 ### 성능 요약
 
-메모리: 75.3 MB, 시간: 0.01 ms
+메모리: 84.8 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 31일 21:01:13
+2025년 10월 12일 18:50:43
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,16 +1,10 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        
-        int sum = brown + yellow; 
-        for(int i=3; i<=brown; i++){
-            if(sum%i==0){
-                int k = sum/i;
-                if(i>=k && (i-2)*(k-2)==yellow){
-                    answer[0] = i;
-                    answer[1] = k;
-                    break;
-                }
+        int sum = brown + yellow;
+        for(int i=3; i * i<= sum; i++){
+            if(sum % i == 0 && (i-2) * (sum/i -2) == yellow){
+                return new int[]{sum/i, i};
             }
         }
         return answer;
