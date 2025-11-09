3b74db24b82774b8549f9005ccdcccf8da67cc73
Park seohyeon <psh0036@suwon.ac.kr>
2025-11-09 23:57:26 +0900

    [level 4] Title: 호텔 방 배정, Time: 373.02 ms, Memory: 153 MB -BaekjoonHub

---
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    Map<Long, Long> parent = new HashMap<>();
+
+    public long[] solution(long k, long[] room_number) {
+        long[] answer = new long[room_number.length];
+
+        for (int i = 0; i < room_number.length; i++) {
+            answer[i] = find(room_number[i]);
+        }
+
+        return answer;
+    }
+
+    private long find(long room) {
+        if (!parent.containsKey(room)) {
+            parent.put(room, room + 1);
+            return room;
+        }
+        long next = find(parent.get(room));
+        parent.put(room, next);
+        return next;
+    }
+}
