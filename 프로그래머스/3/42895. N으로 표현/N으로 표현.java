08166e55381bbd085bbbb19799c90ef98e27fa92
Park seohyeon <psh0036@suwon.ac.kr>
2025-11-29 16:59:20 +0900

    [level 3] Title: N으로 표현, Time: 34.56 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,40 +1,35 @@
 import java.util.*;
 class Solution {
     public int solution(int N, int number) {
-        int num = 0;
+
         List<Set<Integer>> dp = new ArrayList<>();
         for(int i = 0; i<=8; i++){
             dp.add(new HashSet<>());
         }
-        for(int i = 1; i<=8; i++){
-            num = num * 10 + N;
-            dp.get(i).add(num);
+        int catnum = 0;
+        for(int i = 1; i<= 8; i++){
+            catnum = catnum*10 + N;
+            dp.get(i).add(catnum);
         }
+
         for(int i = 1; i<=8; i++){
             Set<Integer> current = dp.get(i);
-            if(current.contains(number)){
-                return i;
-            }
-            for(int k = 1; k<i; k++){
+            for(int k = 1; k < i; k++){
                 Set<Integer> setA = dp.get(k);
                 Set<Integer> setB = dp.get(i-k);
-                if(setA == null || setB == null) continue;
-                for(Integer a : setA){
-                    for(Integer b : setB){
-                        if(a == 0 || b == 0) continue;
-                        current.add(a+b);
-                        current.add(a-b);
-                        current.add(a*b);
-                        current.add(a/b);
-                        if(b!=0) current.add(a/b);
-                        if(a!=0) current.add(b/a);
+                for(Integer A : setA){
+                    for(Integer B : setB){
+                        current.add(A+B);
+                        current.add(A-B);
+                        current.add(B-A);
+                        current.add(A*B);
+                        if(A != 0 && B != 0){
+                            current.add(A/B);
+                        }
                     }
                 }
-
             }
-            if(current.contains(number)){
-                return i;
-            }
+            if(current.contains(number)) return i;
         }
         return -1;
     }
