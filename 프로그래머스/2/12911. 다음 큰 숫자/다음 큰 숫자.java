b7cab94837f6f5e9c4e70472112898d60d2fcb72
Park seohyeon <psh0036@suwon.ac.kr>
2025-12-06 01:38:16 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 54 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,17 +1,9 @@
-import java.util.*;
-
 class Solution {
-    public int solution(int n) {
-        int answer = n+1;
-        int cnt=Integer.bitCount(n);
-        
-        while(true){
-            if(cnt == Integer.bitCount(answer)){
-                break;
-            }
-            else answer++;
-        }
-
-        return answer;
+    public int solution(int x) {
+        int smallest = x & -x;     // rightmost 1 bit
+        int ripple   = x + smallest;
+        int ones     = x ^ ripple;
+        ones = (ones >> 2) / smallest;
+        return ripple | ones;
     }
 }
