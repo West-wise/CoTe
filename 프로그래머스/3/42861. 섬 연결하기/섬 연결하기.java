ad4967efeb3f497249af485379ba535785d473fc
Park seohyeon <psh0036@suwon.ac.kr>
2025-12-28 00:10:20 +0900

    [level 3] Title: 섬 연결하기, Time: 3.79 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,29 +2,26 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        // 프림 알고리즘 사용
-        Map<Integer,List<int[]>> map = new HashMap<>();
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1])); // cost를 기준으로 오름차순 정렬
-        Set<Integer> visited = new HashSet<>();
-
+        boolean[] visited = new boolean[n];
+        Map<Integer, List<int[]>> map = new HashMap<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        
         for(int[] cost : costs){
-            int start = cost[0], next = cost[1], weight = cost[2];
-            map.computeIfAbsent(start, k -> new ArrayList<>()).add(new int[]{next, weight});
-            map.computeIfAbsent(next, k -> new ArrayList<>()).add(new int[]{start, weight});
+            int start = cost[0], end = cost[1];
+            map.computeIfAbsent(start, k -> new ArrayList<>()).add(new int[]{end, cost[2]});
+            map.computeIfAbsent(end, k-> new ArrayList<>()).add(new int[]{start, cost[2]});
         }
-        pq.add(new int[]{0,0}); // 시작노드 설정
-
-        while(!pq.isEmpty() && visited.size() < n){
+        pq.offer(new int[]{0,0});
+        
+        while(!pq.isEmpty()){
             int[] cur = pq.poll();
             int node = cur[0], cost = cur[1];
-            if(visited.contains(node)) continue;
-            visited.add(node);
-            answer += cost;
+            if(visited[node]) continue;
+            visited[node] = true;
+            answer+=cost;
             for(int[] neighbor : map.get(node)){
-                int neighbor_node = neighbor[0], neighbor_cost = neighbor[1];
-                if(!visited.contains(neighbor_node)){
-                    pq.add(new int[]{neighbor_node, neighbor_cost});
-                }
+                int n_node = neighbor[0], n_cost = neighbor[1];
+                if(!visited[n_node]) pq.offer(new int[]{n_node, n_cost});
             }
         }
         return answer;
