764980692c9aa0088a710353591d509f395cd79c
Park seohyeon <psh0036@suwon.ac.kr>
2025-12-29 23:45:52 +0900

    [level 2] Title: 짝지어 제거하기, Time: 6.07 ms, Memory: 6.37 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/README.md
+++ 프로그래머스/2/12973. 짝지어 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 짝지어 제거하기 - 12973 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973?language=cpp) 
 
 ### 성능 요약
 
-메모리: 93.2 MB, 시간: 196.65 ms
+메모리: 6.37 MB, 시간: 6.07 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 02일 21:22:43
+2025년 12월 29일 23:45:50
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+int solution(string s)
+{
+    stack<char> st;
+    for (const char& c : s)
+    {
+        if (st.empty()) st.push(c);
+        else
+        {
+            if (st.top() == c)
+            {
+                st.pop();
+            } else st.push(c);
+        }
+    }
+    return st.empty() ? 1 : 0;
+}
