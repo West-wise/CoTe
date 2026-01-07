2b7607b1ab64f650dffd0cce8e41f6df3b9dfafc
Park seohyeon <psh0036@suwon.ac.kr>
2026-01-07 15:29:32 +0900

    [Gold IV] Title: 호텔, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.cc
+++ 백준/Gold/1106. 호텔/호텔.cc
@@ -1,40 +1,32 @@
-#pragma GCC optimize("O3")
 #include <iostream>
-#include <string>
 #include <vector>
-#include <queue>
-using namespace std;
 
-int main()
-{
-    constexpr int INF = 1'000'000'000;
+using namespace std;
+using pi = pair<int, int>;
+
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-
-    int C, N, max_cus;
+    constexpr int INF = 10000*10000;
+    int C, N;
     cin >> C >> N;
-    max_cus = C + 100;
-    vector<int> dp(max_cus+1, INF); // 100이 최댓값임, 배열의 각 인덱스는 목표 인원을 나타냄
-    dp[0] = 0;
-    vector<pair<int,int>> info(N); // <인원, 비용>의 형태로 저장
-    for (int i= 0; i<N; i++) cin >> info[i].second >> info[i].first;
+    int max_city = C+100;
+    vector<int> dp(max_city+1, INF); dp[0] = 0;
+    vector<pi> info(N);
+    for (auto& [man, cost] : info) cin >> cost >> man;
 
-    for (const auto& [man, cost] : info)
-    {
-        for (int k = 0; k+man<=max_cus; k++)
-        {
-            if (dp[k] == INF) continue;
-            dp[man+k] = min(dp[man+k], dp[k] + cost);
-            // show(dp);
+    for (const auto&[man, cost] : info) {
+        for (int i = 0; i+man<=max_city; i++) {
+            if (dp[i] == INF) continue;
+            dp[i+man] = min(dp[i+man], dp[i] + cost);
         }
     }
     int ans = INF;
-    for (int i=C; i<=max_cus; i++)
-    {
+    for (int i = C; i<=max_city; i++) {
         ans = min(ans, dp[i]);
     }
-    cout << ans << endl;
+    cout << ans;
 
     return 0;
 }
