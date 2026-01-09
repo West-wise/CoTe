d1a88b81122b5cfe66351ef7de277180ed814891
Park seohyeon <psh0036@suwon.ac.kr>
2026-01-09 20:23:48 +0900

    [Gold III] Title: 세 용액, Time: 20 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.cc
+++ 백준/Gold/2473. 세 용액/세 용액.cc
@@ -6,7 +6,8 @@ using namespace std;
 using ll = long long;
 
 int main() {
-
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
     int N;
     cin >> N;
     vector<int> liquid(N);
