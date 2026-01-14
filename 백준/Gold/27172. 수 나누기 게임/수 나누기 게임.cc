c90bbc6b098b0589f1438e8004e15c93d5cd99c6
Park seohyeon <psh0036@suwon.ac.kr>
2026-01-15 00:22:56 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 48 ms, Memory: 10228 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
@@ -3,31 +3,28 @@
 
 using namespace std;
 int main() {
-    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
 
     int N;
     cin >> N;
-    vector<int> pos(1000001, 0), answer(1000001,0);
+    vector<int> pos(1000001), ans(1000001);
     vector<int> card(N);
-
-    for (int i = 0; i<N; i++) {
+    for (int i=0; i<N; i++) {
         cin >> card[i];
-        pos[card[i]]=1;
+        pos[card[i]] = 1;
     }
-    for (int i = 0; i<N; i++) {
-        int cur_num = card[i];
-        for (int k = cur_num; k<=1000000; k+= cur_num) {
-            if (k == cur_num) continue; // 내 카드는 스킵
-            if (pos[k] != 0) { // cur_num의 배수가 존재
-                answer[cur_num]++;
-                answer[k]--;
+    for (int num : card)
+    {
+        for (int i = num; i<1000001; i+=num)
+        {
+            if (i == num) continue;
+            if (pos[i] != 0)
+            {
+                ans[num]++; ans[i]--;
             }
         }
     }
-    for (int val : card) {
-        cout << answer[val] << " ";
-    }
-
+    for (int i : card) cout << ans[i] << " ";
     return 0;
 }
