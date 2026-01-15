ed1dce3565b424a7724a7d151a581f1ac2241b2e
Park seohyeon <psh0036@suwon.ac.kr>
2026-01-15 20:36:33 +0900

    [Gold III] Title: 소수의 연속합, Time: 20 ms, Memory: 18140 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -2,20 +2,18 @@
 #include <vector>
 #include <cmath>
 using namespace std;
-
-
 void get_prime_set(vector<int>& prime_set, int N)
 {
-    vector<int> seive(N+1,0);
-    for (size_t i = 2; i<seive.size(); i++) seive[i] = (int)i;
-    for (int i = 2; i<=sqrt(seive.size()); i++)
+    vector<bool> seive(N+1,true);
+    seive[0] = seive[1] = false;
+    for (int i = 2; i * i <= N; i++)
     {
-        if (seive[i] == 0) continue;
-        for (int k = i*i; k<(int)seive.size(); k+=i) seive[k] = 0;
+        if (!seive[i]) continue;
+        for (int k = i*i; k<=N; k+=i) seive[k] = false;
     }
-    for (size_t i = 2; i<seive.size(); i++)
+    for (int i = 2; i<=N; i++)
     {
-        if (seive.at(static_cast<int>(i)) != 0) prime_set.push_back(seive[i]);
+        if (seive[i] != 0) prime_set.push_back(i);
     }
 }
 
@@ -27,13 +25,16 @@ int main() {
     cin >> N;
     vector<int> prime_set; prime_set.reserve(N+1);
     get_prime_set(prime_set, N+1);
-    int left = 0, right = 0, ans = 0;
-    int sum = prime_set[0];
-    while (left <= right &&  right < N)
+    int left = 0, right = 0, ans = 0, sum = 0;
+    int goal_size = (int)prime_set.size();
+    while (true)
     {
-        if (sum == N) ans++;
-        if (sum < N) sum += prime_set[++right];
-        else sum -= prime_set[left++];
+        if (sum >= N)
+        {
+            if (sum == N) ans++;
+            sum -= prime_set[left++];
+        } else if (right == goal_size) break;
+        else sum += prime_set[right++];
     }
     cout << ans;
 
