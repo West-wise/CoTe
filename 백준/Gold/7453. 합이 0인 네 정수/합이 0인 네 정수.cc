e57517cae6af13c5c91f9dc48077ef7fc57a43ec
Park seohyeon <psh0036@suwon.ac.kr>
2026-01-23 19:30:12 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 2432 ms, Memory: 252184 KB -BaekjoonHub

--- 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
@@ -4,49 +4,60 @@ using namespace std;
 using ll = long long;
 
 int main() {
-    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
 
-    ll N, ans = 0;
+    int N;
+    ll ans = 0;
     cin >> N;
-    vector<vector<ll>> arr(4, vector<ll>(N, 0));
-    for (int i = 0; i<N; i++) {
-        cin >> arr[0][i] >> arr[1][i] >> arr[2][i] >> arr[3][i];
-    }
-    for (auto& row : arr) {
-        sort(row.begin(), row.end());
-    }
-    vector<vector<ll>> tmp_sum(2);
-    tmp_sum[0].reserve(N*N); tmp_sum[1].reserve(N*N);
 
-    for (int num = 0; num<2; num++) {
-        for (int i = 0; i<N; i++) {
-            for (int k = 0; k<N; k++) {
-                tmp_sum[num].push_back(arr[num][i] + arr[num+2][k]);
+    vector<vector<ll>> arr(4, vector<ll>(N));
+
+    for (int i = 0; i<N; i++) cin >> arr[0][i] >> arr[1][i] >> arr[2][i] >> arr[3][i];
+
+    for (auto& v : arr) sort(v.begin(), v.end());
+
+    vector<vector<ll>> sum(2);
+    sum[0].reserve(N*N);
+    sum[1].reserve(N*N);
+
+    for (int num = 0; num < 2; num++)
+    {
+        for (int i = 0; i<N; i++)
+        {
+            for (int k = 0; k<N; k++)
+            {
+                sum[num].push_back(arr[num][i] + arr[num+2][k]);
             }
         }
-        sort(tmp_sum[num].begin(), tmp_sum[num].end());
     }
-    ll left = 0, right = N*N-1;
-    while (left < (ll)tmp_sum[0].size() && right >= 0 ) {
-        ll sum = tmp_sum[0][left] + tmp_sum[1][right];
-        if (sum == 0) {
-            ll cntA = 0, cntB = 0, a = tmp_sum[0][left], b = tmp_sum[1][right];
-            while (left < (ll)tmp_sum[0].size() && tmp_sum[0][left] == a) {
-                cntA++;
-                left++;
-            }
-            while (right < (ll)tmp_sum[1].size() && tmp_sum[1][right] == b) {
-                cntB++;
-                right--;
-            }
+    sort(sum[0].begin(), sum[0].end());
+    sort(sum[1].begin(), sum[1].end());
 
-            ans += cntA * cntB;
+    ll limit = (ll)sum[0].size();
+    ll left = 0, right = limit-1;
+    while (left < limit && right >= 0)
+    {
+        ll sum_val = sum[0][left] + sum[1][right];
+        if (sum_val == 0)
+        {
+            ll cntA=0, cntB=0;
+            ll a = sum[0][left], b = sum[1][right];
+
+            while (left < limit && sum[0][left] == a)
+            {
+                cntA++; left++;
+            }
+            while (right >= 0 && sum[1][right] == b)
+            {
+                cntB++; right--;
+            }
+            ans += cntA*cntB;
         }
-        else if (sum < 0) left++;
+        else if (sum_val < 0) left++;
         else right--;
     }
-
     cout << ans;
+
     return 0;
 }
