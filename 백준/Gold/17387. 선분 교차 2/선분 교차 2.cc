dbd6bf0a1a31130682526a893d2b5a0fd402d146
Park seohyeon <psh0036@suwon.ac.kr>
2026-01-26 16:55:24 +0900

    [Gold II] Title: 선분 교차 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
+++ 백준/Gold/17387. 선분 교차 2/선분 교차 2.cc
@@ -1,26 +1,10 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 using ll = long long;
-
-struct Point
-{
-    ll x,y;
-
-    bool operator <= (const Point &p) const
-    {
-        if (x!=p.x) return x < p.x;
-        return y <= p.y;
-    }
-    bool operator>(const Point& p) const {
-        if (x != p.x) return x > p.x;
-        return y > p.y;
-    }
-};
-
+typedef pair<ll, ll> Point;
 int ccw(Point p1, Point p2, Point p3)
 {
-    ll res = (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
+    ll res = (p2.first - p1.first) * (p3.second - p1.second) - (p2.second - p1.second) * (p3.first - p1.first);
     if (res > 0) return 1;  // 반시계
     if (res < 0) return -1; // 시계
     return 0;               // 일직선
@@ -28,27 +12,23 @@ int ccw(Point p1, Point p2, Point p3)
 
 bool isCross(Point p1, Point p2, Point p3, Point p4)
 {
-    int abc = ccw(p1, p2, p3);
-    int abd = ccw(p1, p2, p4);
-    int cda = ccw(p3, p4, p1);
-    int cdb = ccw(p3, p4, p2);
-
-    if (abc * abd == 0 && cda * cdb == 0)
+    int abc = ccw(p1, p2, p3) * ccw(p1,p2,p4);
+    int cda = ccw(p3, p4, p1) * ccw(p3,p4,p2);
+    if (abc == 0 && cda == 0) // 일직선에 있을 경우
     {
-        if (p1 > p2)swap(p1,p2);
+        if (p1 > p2) swap(p1,p2); // 좌표들을 크기 순으로 정렬한다
         if (p3 > p4) swap(p3,p4);
         return p1 <= p4 && p3 <= p2;
     }
-
-    return (abc * abd) <= 0 && (cda * cdb) <= 0;
+    return abc <= 0 && cda <= 0;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr); cout.tie(nullptr);
     Point p1, p2, p3, p4;
-    cin >> p1.x >> p1.y >> p2.x >> p2.y;
-    cin >> p3.x >> p3.y >> p4.x >> p4.y;
+    cin >> p1.first >> p1.second >> p2.first >> p2.second;
+    cin >> p3.first >> p3.second >> p4.first >> p4.second;
     cout << (isCross(p1,p2,p3,p4) ? 1 : 0);
     return 0;
 }
