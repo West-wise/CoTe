ac765768457f1eb2bae9fbfc5e66de2e1b35c4b5
Park seohyeon <psh0036@suwon.ac.kr>
2026-02-08 02:34:11 +0900

    [Silver I] Title: 안전 영역, Time: 28 ms, Memory: 2684 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.cc
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+constexpr int dx[4] = {-1, 1, 0, 0};
+constexpr int dy[4] = {0, 0, -1, 1};
+int N;
+int dfs(const vector<vector<int>>& area, vector<vector<bool>>& visited, int x, int y, int limit)
+{
+    visited[y][x] = true;
+    int safe_size = 1;
+    for (int dir = 0; dir < 4; dir++)
+    {
+        int nx = x + dx[dir], ny = y + dy[dir];
+        if (nx >= 0 && nx < N && ny >= 0 && ny < N && area[ny][nx] > limit && !visited[ny][nx])
+        {
+            safe_size += dfs(area, visited, nx, ny, limit);
+        }
+    }
+    return safe_size;
+}
+
+
+int main(){
+    cin >> N;
+    vector<vector<int>> area(vector<vector<int>>(N, vector<int>(N, 0)));
+    int ans = 0, max_height = 0;
+    for (int i = 0; i < N; i++)
+    {
+        for (int k = 0; k < N; k++)
+        {
+            cin >> area[i][k];
+            max_height = max(area[i][k], max_height);
+        }
+    }
+
+    for (int limit = 0; limit < max_height; limit++)
+    {
+        vector<vector<bool>> visited(N, vector<bool>(N, false));
+        int tmp = 0;
+        for (int i = 0; i < N; i++) {
+            for (int k = 0; k < N; k++) {
+                if (area[i][k] > limit && !visited[i][k]) {
+                    dfs(area, visited, k, i, limit);
+                    tmp++;
+                }
+            }
+        }
+        ans = max(ans, tmp);
+    }
+    cout << ans << "\n";
+    return 0;
+}
